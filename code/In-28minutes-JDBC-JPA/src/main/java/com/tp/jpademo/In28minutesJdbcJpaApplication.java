ea674b1126cfe881e4d4b1c612812299a7cea248
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-11-11 22:18:36 +0530

    spring-boot basic project code

---
+++ code/In-28minutes-JDBC-JPA/src/main/java/com/tp/jpademo/In28minutesJdbcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.tp.jpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minutesJdbcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28minutesJdbcJpaApplication.class, args);
+	}
+
+}
---
+++ code/In-28minutes-JDBC-JPA/src/test/java/com/tp/jpademo/In28minutesJdbcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28minutesJdbcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
