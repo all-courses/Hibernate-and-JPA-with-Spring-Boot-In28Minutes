ba50d65d891366e55f3d22f87c587b486a13ce52
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-12-02 21:54:16 +0530

    Table Per Class - JPA Inheritance Hierarchies and Mappings

--- code/03_EntitiesAndRelationshipsDemo/src/main/java/com/tp/jpademo/model/Employee.java
+++ code/03_EntitiesAndRelationshipsDemo/src/main/java/com/tp/jpademo/model/Employee.java
@@ -1,16 +1,14 @@
 package com.tp.jpademo.model;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
-@Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "EmployeeType")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@MappedSuperclass
 public abstract class Employee {
 	@Id
 	@GeneratedValue
