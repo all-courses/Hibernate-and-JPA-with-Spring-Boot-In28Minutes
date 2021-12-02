90b72c4d531b48a2616e3a3a82eadb2efcc5e8fd
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-12-02 22:09:09 +0530

    Revert "Joined - JPA Inheritance Hierarchies and Mappings"
    This reverts commit 3136872d209e2543289ff496f352fb8ee968d18e.

--- code/03_EntitiesAndRelationshipsDemo/src/main/java/com/tp/jpademo/model/Employee.java
+++ code/03_EntitiesAndRelationshipsDemo/src/main/java/com/tp/jpademo/model/Employee.java
@@ -1,14 +1,14 @@
 package com.tp.jpademo.model;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
-@Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@MappedSuperclass
 public abstract class Employee {
 	@Id
 	@GeneratedValue
@@ -21,7 +21,7 @@ public abstract class Employee {
 		super();
 	}
 
-	protected Employee(String fullName) {
+	public Employee(String fullName) {
 		super();
 		this.fullName = fullName;
 	}
