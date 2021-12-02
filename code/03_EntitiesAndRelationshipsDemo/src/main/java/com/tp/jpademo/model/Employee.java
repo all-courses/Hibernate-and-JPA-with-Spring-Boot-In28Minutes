e309bd4a220292e10fd89d4d3b3e56acf8c966b8
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-12-02 22:20:08 +0530

    Mapped Super Class - JPA Inheritance Hierarchies and Mappings

--- code/03_EntitiesAndRelationshipsDemo/src/main/java/com/tp/jpademo/model/Employee.java
+++ code/03_EntitiesAndRelationshipsDemo/src/main/java/com/tp/jpademo/model/Employee.java
@@ -1,14 +1,11 @@
 package com.tp.jpademo.model;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
-@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@MappedSuperclass
 public abstract class Employee {
 	@Id
 	@GeneratedValue
